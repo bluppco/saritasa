5c7b291fdcff885bd29a2dea299e701234c9e13c
Tanish Sharma <tanishsharmaab17@gmail.com>
2024-02-14 12:15:21 +0530

    feat: added shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,12 +7,15 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind(), react()],
-  output: "server",
-  adapter: cloudflare({
-    runtime: {
-      mode: 'local',
-      type: 'pages'
-    }
-  })
+	integrations: [
+		mdx(),
+		tailwind(),
+		react()],
+	output: "server",
+	adapter: cloudflare({
+		runtime: {
+			mode: 'local',
+			type: 'pages'
+		}
+	})
 });
