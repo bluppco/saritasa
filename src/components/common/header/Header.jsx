985aa8efe19aa426cb406126d56145c7d5206a7d
Tanish Sharma <tanishsharmaab17@gmail.com>
2024-02-14 12:35:25 +0530

    feat: added animated underline in header

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -44,43 +44,47 @@ const Header = ( props ) => {
                                     header_data.map( ( item, index ) => {
 
                                         return (
-                                            <li key={ index } className={` ${ activePage === item.data.title ? "border-saritasa_primary" : "border-transparent" } pb-1 border-b-2 text-base text-slate-500 font-avenir font-semibold uppercase flex items-center gap-2 relative group cursor-pointer`} onClick={() => setActivePage( item.data.title )}>
-                                                <a href={ "/" + item.data.slug } className="group-hover:text-saritasa_black" aria-label={"explore " + item.data.title }>{ item.data.title }</a>
-                                                {
+                                            <li className="group">
+                                                <div key={ index } className={` ${ activePage === item.data.title ? "border-saritasa_primary" : "border-transparent" } pb-1 border-b-2 text-base text-slate-500 font-avenir font-semibold uppercase flex items-center gap-2 relative group cursor-pointer`} onClick={() => setActivePage( item.data.title )}>
 
-                                                    item.data.has_dropdown &&
-                                                    <div>
-                                                        <img
-                                                            src="/icons/arrow_down.svg"
-                                                            className="w-3 aspect-square"
-                                                            alt="dropdown menu icon"
-                                                        />
-                                                        <div className="hidden group-hover:block group-hover:flex-col absolute p-6 bg-white left-0 z-20 top-7 w-[460px] gap-2 shadow transition-all duration-300 delay-1000">
-                                                            <div className="grid grid-cols-2 gap-x-4">
-                                                                {
+                                                    <a href={ "/" + item.data.slug } className="group-hover:text-saritasa_black" aria-label={"explore " + item.data.title }>{ item.data.title }</a>
+                                                    {
 
-                                                                    item.data.items.map( ( sub_item, sub_item_index ) => {
+                                                        item.data.has_dropdown &&
+                                                        <div>
+                                                            <img
+                                                                src="/icons/arrow_down.svg"
+                                                                className="w-3 aspect-square"
+                                                                alt="dropdown menu icon"
+                                                            />
+                                                            <div className="hidden group-hover:block group-hover:flex-col absolute p-6 bg-white left-0 z-20 top-7 w-[460px] gap-2 shadow transition-all duration-300 delay-1000">
+                                                                <div className="grid grid-cols-2 gap-x-4">
+                                                                    {
 
-                                                                        return(
-                                                                            <div className="py-1">
-                                                                                <a
-                                                                                    href={ "/" + item.data.slug + "/" + sub_item.slug }
-                                                                                    className="text-slate-700 hover:text-saritasa_black hover:underline"
-                                                                                    aria-label={ "explore" + sub_item.title }
-                                                                                >
-                                                                                    <div className="font-light capitalize tracking-wide text-t font-avenir">{ sub_item.title }</div>
-                                                                                </a>
-                                                                            </div>
-                                                                        )
+                                                                        item.data.items.map( ( sub_item, sub_item_index ) => {
 
-                                                                    })
+                                                                            return(
+                                                                                <div className="py-1">
+                                                                                    <a
+                                                                                        href={ "/" + item.data.slug + "/" + sub_item.slug }
+                                                                                        className="text-slate-700 hover:text-saritasa_black hover:underline"
+                                                                                        aria-label={ "explore" + sub_item.title }
+                                                                                    >
+                                                                                        <div className="font-light capitalize tracking-wide text-t font-avenir">{ sub_item.title }</div>
+                                                                                    </a>
+                                                                                </div>
+                                                                            )
 
-                                                                }
+                                                                        })
+
+                                                                    }
+                                                                </div>
                                                             </div>
                                                         </div>
-                                                    </div>
 
-                                                }
+                                                    }
+                                                </div>
+                                                <div class="w-0 bg-saritasa_primary group-hover:w-full h-[2px] transition-width duration-200 ease-in"></div>
                                             </li>
                                         )
 
